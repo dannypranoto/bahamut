ae96482b8d1a018b984401a2f6b01265bd057129
initialds <initia_ds@github.com>
2014-10-17 22:44:17 +0700

    generate views for customers and finishing init db

--- db/migrate/20141017045435_user_attribute_init.rb
+++ db/migrate/20141017045435_user_attribute_init.rb
@@ -2,7 +2,7 @@ class UserAttributeInit < ActiveRecord::Migration
   def change
   	add_column :customers, :username, :string, :null => false
   	add_column :customers, :balance, :integer, default: 0
-  	add_column :customers, :status, :integer
-
+  	add_column :customers, :status_customer, :boolean , :default => true 
+	add_column :customers, :status_increase_limit, :boolean , :default => false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017044329) do
+ActiveRecord::Schema.define(version: 20141017045435) do
 
   create_table "customers", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username",                               null: false
+    t.integer  "balance",                default: 0
+    t.boolean  "status_customer",        default: true
+    t.boolean  "status_increase_limit",  default: false
   end
 
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true, using: :btree
