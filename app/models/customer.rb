8ae60aaf215583664b8de90188423fb84418001e
initialds <initia_ds@github.com>
2014-10-18 12:30:45 +0700

    edit profile done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ def email=(address)
+    if new_record?
+      write_attribute(:email, address)
+    else
+      raise 'email is immutable!'
+    end
+  end 
 end
